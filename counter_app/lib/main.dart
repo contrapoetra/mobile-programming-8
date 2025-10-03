4d8c3b027b67545acb6732c0acd37992d4b7b496
Dimas Atha Putra <contrapoetra@proton.me>
2025-10-03 15:33:36 +0700

    write minimal counter thing

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -1,4 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Stateful Widget Demo',
+      home: MyHomePage(title: 'Counter App'),
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0; // state
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter App')),
+      body: Center(
+        child: Text(
+          '$_counter',
+          style: const TextStyle(fontSize: 40),
+        ),
+      ),
+    );
+  }
 }
